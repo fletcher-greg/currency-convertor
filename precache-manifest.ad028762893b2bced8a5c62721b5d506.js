2164c60e34ee404043073018d40dc6ac283895aa
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-06-30 15:21:46 +0200

    Updates

---
+++ precache-manifest.ad028762893b2bced8a5c62721b5d506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76542f31b7902c45b9396d3ba325e831",
+    "url": "/fletcher-greg/currency-convertor/index.html"
+  },
+  {
+    "revision": "bc8721ba55afe262fd03",
+    "url": "/fletcher-greg/currency-convertor/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f0b521c66bc702a37631",
+    "url": "/fletcher-greg/currency-convertor/static/js/2.9f4f2337.chunk.js"
+  },
+  {
+    "revision": "bc8721ba55afe262fd03",
+    "url": "/fletcher-greg/currency-convertor/static/js/main.b36a0c2a.chunk.js"
+  },
+  {
+    "revision": "8d37bde6b6999ed5e4af",
+    "url": "/fletcher-greg/currency-convertor/static/js/runtime~main.d4b4261d.js"
+  }
+]);
